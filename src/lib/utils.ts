57cc5b7afc86dba3512c0baf079f9dbf1d345ed4
xiaoxiao-cvs <2573270370@qq.com>
2026-02-28 00:42:10 +0800

    feat(lib): 添加 cn 工具函数与通用辅助模块
    - 基于 clsx + tailwind-merge 实现 cn() 类名合并工具
    - 供 COSS UI 组件和自定义组件使用

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
-import { type ClassValue, clsx } from "clsx";
+import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+/** Merge Tailwind classes with clsx conditional support. */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
